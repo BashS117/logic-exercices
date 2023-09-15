b3db201885903e7f7effca9627d206cb18c3f73c
Sebastian Rodriguez <sebasrm2000@gmail.com>
2023-09-14 22:48:48 -0500

    consumir APIREST, fetch

---
+++ APIs/ApiRestyFetch.js
@@ -0,0 +1,13 @@
+const options={
+    method:"GET",
+    headers:{
+        'X-RapidAPI-key': sumOfDoubledElements,
+        'X-RapidAPI-Host ': sumOfDoubledElements,
+    }
+}
+
+fetch("https://kogezxjfuixqqiarnnxo.supabase.co/rest/v1/Questions?select=*", options)
+.then(res=>res.json())
+.then(response=>{
+    console.log(response)
+})
