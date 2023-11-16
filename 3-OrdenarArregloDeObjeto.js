d07f0a0ca8730eb0e9798b56b827ac6e7e44db5d
Sebastian Rodriguez <sebasrm2000@gmail.com>
2023-11-15 22:44:50 -0500

    sortObject excercise

--- 3-OrdenarArregloDeObjeto.js
+++ 3-OrdenarArregloDeObjeto.js
@@ -3,7 +3,7 @@
 function sortObjects(arr, property) {
     arr.sort((a,b)=>a[property] - b[property]);
 }
-const people= [
+const peoples= [
     {name:"Alice", age: 30},
     {name:"Bob", age: 25},
     {name:"Charlie", age: 35},
@@ -13,10 +13,26 @@ const people= [
 // sortObjects(people, "age");
 console.log(people)
 
-
+///segundo intento
 function ordenarObjetos(arr, propiedad){
     arr.sort((a,b)=>a[propiedad]-b[propiedad])
 }
 
 ordenarObjetos(people, "age");
+console.log(people)
+//3er practica
+
+function sortearObjects(arr, property){
+    arr.sort((a,b)=>a[property]-b[property]);
+}
+
+const people= [
+    {name:"Alice", age: 30},
+    {name:"Bob", age: 25},
+    {name:"Charlie", age: 35},
+
+]
+
+
+sortearObjects(people, "age");
 console.log(people)
